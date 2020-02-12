5ce04921432076ee894f1d3b638790f42622c654
Pin <43724150+PINYI1@users.noreply.github.com>
2020-02-12 18:22:11 +0000

    refined version

--- Problem Set 1/credit.c
+++ Problem Set 1/credit.c
@@ -5,103 +5,48 @@ int main(void)
 {
     int count = 0;
     long number = get_long("Number : ");
-    long num = number;
-    int odd_digit = 0;
+    int digit1 = 0;
+    int digit2 = 0;
     int odd_sum = 0;
-    int even_digit = 0;
     int even_sum = 0;
-    int initial_digit;
-    while (num != 0)
+    
+    while (number != 0)
     {
-        /* Increment digit count */
         count++;
-
-        /* Remove last digit of 'num' */
-        num /= 10;
+        digit2 = digit1;
+        digit1 = number % 10;
+        
+        if (count % 2 == 1)
+        {
+            odd_sum += digit1; 
+        }
+        else
+        {
+            int multiple = digit1 * 2;
+            even_sum += multiple / 10 + multiple % 10;
+        }
+        number /= 10;
     }
-    if (count < 13)
+    
+    int test_num = (odd_sum + even_sum) % 10;
+    int initial_digit = digit1 * 10 + digit2;
+    printf("Initial_dg: %d\n", initial_digit);
+    printf("Count: %d\n", count);
+    if (test_num == 0 &&(initial_digit == 34 || initial_digit == 37) && (count == 15))
+    {
+        printf("AMEX\n");
+    }
+    else if (test_num == 0 && initial_digit >= 51 && initial_digit <= 55 && count == 16)
+    {
+        printf("MASTERCARD\n");
+    }
+    else if (test_num == 0 && digit1 == 4 && (count == 13 || count == 16))
+    {
+        printf("VISA\n");
+    }
+    else 
     {
         printf("INVALID\n");
     }
-    else
-    {
-        for (int i = 1; i <= count; i++)
-        {
-            if (i % 2 == 1)
-            {
-                odd_digit = number % 10;
-                odd_sum += odd_digit; 
-                number /= 10;
-            }
-            else
-            {
-                even_digit = (number % 10) * 2;
-                if (even_digit > 9)
-                {
-                    int loop_digit = even_digit;
-                    int sub_digit = 0;
-                    while (loop_digit)
-                    {
-                        sub_digit += loop_digit % 10;
-                        loop_digit /= 10;
-                    }
-                    even_sum += sub_digit; 
-                    number /= 10;
-                }
-                else
-                {
-                    even_sum += even_digit; 
-                    number /= 10;
-                } 
-            }
-        }
-        int test_num = (odd_sum + even_sum) % 10;
-        if (test_num == 0)
-        {
-            if (count % 2 == 0)
-            {
-                initial_digit = even_digit / 2 * 10 + odd_digit;
-                if (initial_digit == 34 || initial_digit == 37)
-                {
-                    printf("AMEX\n");
-                }
-                else if (initial_digit == 51 || initial_digit == 52 || initial_digit == 53 || initial_digit == 54 || initial_digit == 55)
-                {
-                    printf("MASTERCARD\n");
-                }
-                else if (even_digit == 8)
-                {
-                    printf("VISA\n");
-                }
-                else 
-                {
-                    printf("INVALID\n");
-                }
-            }
-            else
-            {
-                initial_digit = odd_digit * 10 + even_digit / 2;
-                if (initial_digit == 34 || initial_digit == 37)
-                {
-                    printf("AMEX\n");
-                }
-                else if (initial_digit == 52 || initial_digit == 53 || initial_digit == 54 || initial_digit == 55)
-                {
-                    printf("MASTERCARD\n");
-                }
-                else if (odd_digit == 4)
-                {
-                    printf("VISA\n");
-                }
-                else 
-                {
-                    printf("INVALID\n");
-                }
-            }
-        }
-        else 
-        {
-            printf("INVALID\n");
-        }
-    }
+            
 }
